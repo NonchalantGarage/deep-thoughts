0b3266a9c2aad8f5566f4692f2a286caf39a9690
curtis <hong.curtis@gmail.com>
2022-03-23 08:46:47 -0700

    added JWT token and auth

--- schemas/resolvers.js
+++ schemas/resolvers.js
@@ -1,4 +1,6 @@
 const {User, Thought} = require('../models')
+const{AuthenticationError} = require('apollo-server-express');
+const {signToken} = require('../utils/auth');
 
 
 const resolvers = {
@@ -22,7 +24,34 @@ const resolvers = {
             .populate('friends')
             .populate('thoughts');
         }
+        
+    },
 
+    Mutation: {
+        // create method for User model using arguments passed into args
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+          
+            return {token, user} ;
+          },
+        //   validation for to test email and password 
+        login: async (parent,{email,password}) =>{
+            const user = await User.findOne({email});
+
+            if(!user){
+                throw new AuthenticationError('Incorrect credentials')
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw){
+                throw new AuthenticationError('Incorrect credentials')
+            }
+
+            const token = signToken(user);
+            return {token, user};
+        }
     }
 };
 
--- schemas/typeDefs.js
+++ schemas/typeDefs.js
@@ -1,9 +1,19 @@
 // import the gql tagged template function
 
+
 const { gql } = require("apollo-server-express");
 
 // create our typeDefs
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
+
   type Thought {
     _id: ID
     thoughtText: String
@@ -12,6 +22,7 @@ const typeDefs = gql`
     reactionCount: Int
     reactions: [Reaction]
   }
+
   type Reaction {
     _id: ID
     reactionBody: String
@@ -20,25 +31,23 @@ const typeDefs = gql`
   }
 
   type Query {
-    thoughts(username: String): [Thought]
-  }
-
-  type User {
-    _id: ID
-    username: String
-    email: String
-    friendCount: Int
-    thoughts:[Thought]
-    friends: [User]
-  }  
-  
-  type Query {
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
   }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
 `;
+
 
 // export the typeDefs
 module.exports = typeDefs;
